26186230f588d80dbb068d079f4c107cb37d4d24
VTPartner <vtpartner7@gmail.com>
2025-04-23 12:55:14 +0530

    Initial Commit

---
+++ KAPS/KAPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KAPSApp.swift
+//  KAPS
+//
+//  Created by Shaheed on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct KAPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KAPSTests/KAPSTests.swift
@@ -0,0 +1,17 @@
+//
+//  KAPSTests.swift
+//  KAPSTests
+//
+//  Created by Shaheed on 23/04/25.
+//
+
+import Testing
+@testable import KAPS
+
+struct KAPSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
